d3a653e068ff55f3568e59149b18775cf3661616
caozhao <magiciancz2020@gmail.com>
2017-08-03 18:29:02 +0800

    1

---
+++ CIS_TEST/Assets/CIS_AutoBuildConfig/Editor/AutoBuildRawTemplate.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace CIS
+{
+	public class AutoBuildRawTemplate : AutoBuildBaseRawTemplate {
+		//add other field here, make sure they are public
+		//public Color OtherField;//demonstrate here, please remove it 
+
+		public override void ApplyRawConfigData(string channelname)
+		{
+			//Apply your own data into game
+			base.ApplyRawConfigData(channelname);
+		}
+
+		public static void ClearRawConfigData()
+		{
+			
+		}
+
+
+	}
+}
+
